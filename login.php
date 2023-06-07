a0b86973691a2f63666e236c60395fbe6e7baac6
thearvindk <117643735+thearvindk@users.noreply.github.com>
2023-06-07 10:33:22 +0530

    css 3.0

--- login.php
+++ login.php
@@ -72,12 +72,12 @@ function test_input($data)
     <title>Login</title>
     <link rel="stylesheet" href="css/styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Trirong">
 </head>
 
 <body>
     <div class="container">
     <div class="lbox">
+        <div class="lmod">
             <img src="assets/images.png" class="logo" alt="ONGC Logo" />
             <h1>Gate Pass Portal for ONGCians</h1>
             <h2>Login</h2>
@@ -97,6 +97,8 @@ function test_input($data)
                 <?php endif; ?>
             </form>
             <p>Don't have an account? <a href="signup.php">Sign Up</a></p>
+    
+        </div>
     </div>
     </div>
 </body>
