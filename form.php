728502424a7501c9a70e2c938f2342d055c4cfeb
Ayan Ghazi <ayan.ghazi007@gmail.com>
2023-06-08 14:37:35 +0530

    made the add row button work

--- form.php
+++ form.php
@@ -108,7 +108,6 @@ function getEmployeesByDesignation($designation)
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/styles.css">
-    <script type="text/javascript" src="/form.js"></script>
 </head>
 
 <body>
@@ -180,30 +179,7 @@ function getEmployeesByDesignation($designation)
         <br>
         <button type="submit">Submit</button>
     </form>
-    <!-- <script>
-    document.getElementById("addRowBtn").addEventListener("click", function() {
-        var table = document.getElementById("myTable");
-        var row = table.insertRow();
-        var cells = [];
-
-        for (var i = 0; i < 5; i++) {
-            cells[i] = row.insertCell(i);
-            cells[i].innerHTML = `<input type="text" name="srno[]">`;
-        }
-
-        var deleteButtonCell = row.insertCell(5);
-        deleteButtonCell.innerHTML = `<button class="removeRowBtn" type="button">Remove</button>`;
-    });
-
-    // Event delegation to handle remove button clicks
-    document.addEventListener("click", function(event) {
-        if (event.target.classList.contains("removeRowBtn")) {
-            var row = event.target.parentNode.parentNode;
-            row.parentNode.removeChild(row);
-        }
-    });
-</script> -->
-
+<script type="text/javascript" src="form.js"></script>
 </body>
 
 </html>
