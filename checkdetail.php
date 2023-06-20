46c27af0609bce5da34986854aad06874c144c2e
thearvindk <117643735+thearvindk@users.noreply.github.com>
2023-06-20 16:00:39 +0530

    checkdetail changes

--- checkdetail.php
+++ checkdetail.php
@@ -7,10 +7,7 @@ $conn=$connection;
 $cpfno = $_GET['cpfno'];
 
 // Prepare and execute a SQL query to fetch the matching users
-$stmt = $conn->prepare("SELECT u.username, u.email, u.cpfno FROM users u 
-    WHERE u.cpfno LIKE ? AND u.id IN (
-    SELECT ug.id FROM user_to_groups ug WHERE ug.group_id = 2
-)");
+$stmt = $conn->prepare("Select * from employee where cpfno like ?");
 $stmt->bind_param("s", $searchValue);
 $searchValue = $cpfno . '%';
 $stmt->execute();
