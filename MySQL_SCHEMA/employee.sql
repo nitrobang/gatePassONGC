7e6d2316d5fbc51171dc5225306e8572645c3cb8
ChiragS007 <chiragsharmat@gmail.com>
2023-06-21 14:02:17 +0530

    commit

--- MySQL_SCHEMA/employee.sql
+++ MySQL_SCHEMA/employee.sql
@@ -1,15 +1,14 @@
---O is for ONGC employee(Store Keeper and Collector) and G is for Security Guard
 CREATE TABLE employee (
   cpfno INT NOT NULL,
   empname VARCHAR(100) NOT NULL,
-  designation ENUM('E','G','S') NOT NULL, 
+  designation ENUM('E','S') NOT NULL, 
   venue ENUM('N', 'V', 'H') NOT NULL, 
   PRIMARY KEY (cpfno)
 );
 
 INSERT INTO employee (cpfno, empname, designation, venue) VALUES
-(123456, 'John Doe', 'O', 'N'),
+(123456, 'John Doe', 'E', 'N'),
 (789012, 'Jane Smith', 'G', 'V'),
-(345678, 'Michael Johnson', 'O', 'H'),
+(345678, 'Michael Johnson', 'E', 'H'),
 (901234, 'Emily Davis', 'G', 'N'),
-(567890, 'David Wilson', 'O', 'V');
+(567890, 'David Wilson', 'E', 'V');
--- db_connection.php
+++ db_connection.php
@@ -6,8 +6,8 @@ if (session_status() === PHP_SESSION_NONE) {
 // Create a MySQL database connection
 $servername = "localhost";
 $username = "root";
-$password = "Qweasdzxc@007";
-$dbname = "gate_pass_new";
+$password = "";
+$dbname = "gatepass_new";
 $connection = mysqli_connect($servername, $username, $password, $dbname);
 
 // Check if the connection was successful
