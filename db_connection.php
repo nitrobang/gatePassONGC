1d6da9c12e6659cb2d2a138cbdc24d219f202cc8
ChiragS007 <chiragsharmat@gmail.com>
2023-07-07 12:14:03 +0530

    collector page drop down

--- db_connection.php
+++ db_connection.php
@@ -7,7 +7,7 @@ if (session_status() === PHP_SESSION_NONE) {
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "gate_pass_new";
+$dbname = "gatepass_new";
 $connection = mysqli_connect($servername, $username, $password, $dbname);
 
 // Check if the connection was successful
